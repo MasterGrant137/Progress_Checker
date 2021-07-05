79d71b16b831fd072911051d99a577b6cf96e5d7
Alejandro C. Grant <authorcgrant@gmail.com>
2021-07-05 12:54:52 -0700

    js is behaving and meshed well with CSS, needs formatting

--- script.js
+++ script.js
@@ -7,32 +7,37 @@ const newRow = () => {
     let newRowButton = document.querySelector("#new-row");
 
     newRowButton.addEventListener("click", event => {
-        let row = document.createElement("div");
-        row.setAttribute("id", `row-${rowCount}`);
-        row.setAttribute("class", "row");
-        main.appendChild(row);
+        let rowL = document.createElement("div");
+        rowL.setAttribute("id", `rowL-${rowCount}`);
+        rowL.setAttribute("class", "row");
+        leftSideBar.appendChild(rowL);
+
+        let rowM = document.createElement("div");
+        rowM.setAttribute("id", `rowM-${rowCount}`);
+        rowM.setAttribute("class", "row");
+        main.appendChild(rowM);
 
         let removeRow = document.createElement("p");
         removeRow.setAttribute("class", "remove-row");
         removeRow.innerText = "✂️";
-        row.appendChild(removeRow);
+        rowL.appendChild(removeRow);
 
         let date = document.createElement("input");
         date.setAttribute("type", "date");
         date.setAttribute("class", "date");
-        row.appendChild(date);
+        rowL.appendChild(date);
 
         let minus = document.createElement("button");
         minus.setAttribute("id", `minus-${rowCount}`);
         minus.setAttribute("class", "minus");
         minus.innerText = "-";
-        row.appendChild(minus);
+        rowL.appendChild(minus);
 
         let plus = document.createElement("button");
         plus.setAttribute("id", `plus-${rowCount}`);
         plus.setAttribute("class", "plus");
         plus.innerText = "+";
-        row.appendChild(plus);
+        rowL.appendChild(plus);
 
         plus.addEventListener("click", event => {
             let checkboxContainer = document.createElement("span");
@@ -42,7 +47,7 @@ const newRow = () => {
             checkbox.setAttribute("type", "checkbox");
             checkbox.setAttribute("class", "checkbox")
 
-            row.appendChild(checkboxContainer);
+            rowM.appendChild(checkboxContainer);
             checkboxContainer.appendChild(checkbox);
 
             counters(rowCount);
@@ -55,22 +60,26 @@ const newRow = () => {
 
 
 const counters = (rowCount) => {
-    // console.log(runningRowCount);
-    // let sidebar = document.querySelector("#sidebar");
-    // let currentRowCount = document.querySelector(`#row-${runningRowCount}`);
-    // console.log(runningRowCount)
-    // let countArea = document.createElement("div");
-    // countArea.setAttribute("id", `count-area-${runningRowCount}`);
-    // countArea.setAttribute("class", "count-area");
+    let countAreaExists = document.querySelector(`#count-area-${rowCount}`);
+    let rightSidebar = document.querySelector("#right-sidebar");
+    
+    if (!countAreaExists) {
+        let newCountArea = document.createElement("div");
+        newCountArea.setAttribute("id", `count-area-${rowCount}`);
+        newCountArea.setAttribute("class", "count-area");
+
+        countAreaExists = newCountArea;
+    }
 
 
-    //! let testCount = document.querySelector(`#row-0`).children.length;
-    // console.log(rowCount - 1)
-    testCount = document.querySelector(`#row-${rowCount - 1}`).children.length;
-    console.log(testCount);
-    // let rowCheckboxCount = currentRow.children.length;
-    // countArea.innerText = rowCheckboxCount;
-    // sidebar.appendChild(countArea);
+
+    let rowLength = document.querySelector(`#rowM-${rowCount - 1}`).children.length;
+    console.log(countAreaExists);
+
+    // countAreaExists.innerText = "";
+    countAreaExists.innerText = rowLength;
+
+    rightSidebar.appendChild(countAreaExists);
 }
 
 window.addEventListener("DOMContentLoaded", () => {
