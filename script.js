78be205bae1900c8d4acc4eb18707e0af55da91b
Alejandro C. Grant <authorcgrant@gmail.com>
2021-07-06 23:53:55 -0700

    big moves made with local storage

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 let rowCount = 0;
 let checkboxCount = 0;
+let eleObject = {};
+
+const renderPage = () => {
+    // let leftSideBarKids = document.querySelector("#left-sidebar").children;
+    // let existingDate = localStorage[``]
+
+    // leftSideBarKids.forEach((rowL, i) => {
+
+    // });
+}
 
 const newRow = () => {
     let leftSideBar = document.querySelector("#left-sidebar");
@@ -7,23 +17,11 @@ const newRow = () => {
     let rightSidebar = document.querySelector("#right-sidebar");
     let newRowButton = document.querySelector("#new-row");
 
-    newRowButton.addEventListener("click", event => {
+    newRowButton.addEventListener("click", () => {
         let rowL = document.createElement("div");
         rowL.setAttribute("id", `rowL-${rowCount}`);
         rowL.setAttribute("class", "row");
         leftSideBar.appendChild(rowL);
-
-        let rowM = document.createElement("div");
-        rowM.setAttribute("id", `rowM-${rowCount}`);
-        rowM.setAttribute("class", "row");
-        main.appendChild(rowM);
-        let rowLength = document.querySelector(`#rowM-${rowCount}`).children.length;
-
-        let rowR = document.createElement("div");
-        rowR.setAttribute("id", `rowR-${rowCount}`);
-        rowR.setAttribute("class", "row");
-        rowR.innerText = rowLength;
-        rightSidebar.appendChild(rowR);
 
         let removeRow = document.createElement("span");
         removeRow.setAttribute("class", "remove-row");
@@ -32,8 +30,23 @@ const newRow = () => {
 
         let date = document.createElement("input");
         date.setAttribute("type", "date");
+        date.setAttribute("id", `date-${rowCount}`);
         date.setAttribute("class", "date");
         rowL.appendChild(date);
+
+        const storeDate = () => {
+           let currInput = date.id;
+           let dateVal = date.value;
+
+           eleObject["currDateObject"] = dateVal;
+           localStorage.setItem(currInput, JSON.stringify(eleObject));
+           localStorage.removeItem()
+
+        }
+
+        date.addEventListener("change", storeDate);
+        // date.addEventListener("keyup", storeDate);
+        // date.addEventListener("click", storeDate);
 
         let minus = document.createElement("button");
         minus.setAttribute("id", `minus-${rowCount}`);
@@ -47,7 +60,7 @@ const newRow = () => {
         plus.innerText = "+";
         rowL.appendChild(plus);
 
-        plus.addEventListener("click", event => {
+        plus.addEventListener("click", () => {
             let checkbox = document.createElement("input");
             checkbox.setAttribute("type", "checkbox");
             rowLength++;
@@ -55,10 +68,24 @@ const newRow = () => {
             rowM.appendChild(checkbox);
         });
 
+        let rowM = document.createElement("div");
+        rowM.setAttribute("id", `rowM-${rowCount}`);
+        rowM.setAttribute("class", "row");
+        main.appendChild(rowM);
+        let rowLength = document.querySelector(`#rowM-${rowCount}`).children.length;
+
+        let rowR = document.createElement("div");
+        rowR.setAttribute("id", `rowR-${rowCount}`);
+        rowR.setAttribute("class", "row");
+        rowR.innerText = rowLength;
+        rightSidebar.appendChild(rowR);
+
         rowCount++;
+
     })
 }
 
 window.addEventListener("DOMContentLoaded", () => {
+    renderPage();
     newRow();
 })
